e1945a1e3fd245b5adb8dd51c8583b2531d6b1f8
susara-perera <susaraperera33@gmail.com>
2024-09-24 13:13:43 +0530

    ex1
    git

--- Full Project/frontend/src/Components/Payment/components/AddPayment.js
+++ Full Project/frontend/src/Components/Payment/components/AddPayment.js
@@ -10,7 +10,7 @@ function AddPayment() {
     cardNumber: "",
     date: "",
     cvc: "",
-    description: "", // Added description field ex1
+    description: "", // Added description field ex1(git)
   });
 
   const handleChange = (e) => {

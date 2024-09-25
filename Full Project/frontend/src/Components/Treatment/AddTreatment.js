20fa010c41b47e4f7c9cf451e977e966f8a34912
Sahan Udara <76866708+Sahan-Udara@users.noreply.github.com>
2024-09-25 13:03:25 +0530

    headerfooter nurse
    headerfooter nurse

--- Full Project/frontend/src/Components/Treatment/AddTreatment.js
+++ Full Project/frontend/src/Components/Treatment/AddTreatment.js
@@ -104,7 +104,7 @@ function AddTreatment() {
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 H O S P I T A L</div>
                 {/* <button className="login-btnAd">Log Out</button> */}
-                <button className="login-btnAd" onClick={() => navigate('/Header')}>Log Out</button>
+                <button className="login-btnAd" onClick={() => navigate('/adminhome')}>Log Out</button>
 
             </header>
        {/* Home Header End */}

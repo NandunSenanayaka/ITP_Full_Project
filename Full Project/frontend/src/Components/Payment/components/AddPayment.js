f066432b5b73d6b2adf04390642a6fc1b36453da
susara-perera <susaraperera33@gmail.com>
2024-09-29 21:35:41 +0530

    addPayment.
    remove comment.

--- Full Project/frontend/src/Components/Payment/components/AddPayment.js
+++ Full Project/frontend/src/Components/Payment/components/AddPayment.js
@@ -10,7 +10,7 @@ function AddPayment() {
     cardNumber: "",
     date: "",
     cvc: "",
-    description: "", // Added description field ex1(git)
+    description: "", 
   });
 
   const handleChange = (e) => {
